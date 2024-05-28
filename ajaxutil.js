842d6d994038d00db1adb0c671997326342cdcd4
Shaheer Kashif <153809729+Shaheer-Kashif@users.noreply.github.com>
2024-05-28 16:20:40 +0500

    Ajax Deployed

---
+++ ajaxutil.js
@@ -0,0 +1,39 @@
+(function (global) {
+    var ajaxUtils = {};
+
+    function getRequest() {
+        if (global.XMLHttpRequest) {
+            return (new XMLHttpRequest());
+        }
+        else {
+            global.alert("Ajax is not supported");
+            return (null);
+        }
+    }
+
+    ajaxUtils.sendGetRequest = function(requestUrl, responseHandler, isJsonResponse) {
+        var request = getRequest();
+        request.onreadystatechange = function () {
+            handleResponse(request,responseHandler,isJsonResponse);
+        };
+        request.open("GET",requestUrl,true);
+        request.send(null);
+    };
+    
+    function handleResponse(request,responseHandler,isJsonResponse) {
+        if ((request.readyState == 4) && (request.status == 200)) {
+            if (isJsonResponse == undefined) {
+                isJsonResponse = true;
+            }
+            if (isJsonResponse) {
+                responseHandler(JSON.parse(request.responseText));
+            }
+            else {
+                responseHandler(request.responseText);
+            }
+        }
+    }
+
+    global.$ajaxUtils = ajaxUtils;
+
+})(window);
--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-ajaxutil = {};
-console.log("Hello This is Java Script");
+document.addEventListener("DOMContentLoaded",function (event) {
+    document.querySelector("button").addEventListener("click",function () {
+        $ajaxUtils.sendGetRequest("data/data.json", function (res) {
+            var name = res.firstname + " " + res.lastname;
+            var message = "";
+            if (res.status) {
+                message += "is Happy!";
+            }
+            else {
+                message += "is in Pain...";
+            }
+            document.querySelector("#content").innerHTML = "<h2>"+name +" "+message + "</h2>";
+
+        }
+    );
+    });
+
+}
+);
